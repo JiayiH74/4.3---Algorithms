34b7875c7f8b42b9f2ccdbf4ec9984f3adea6d2a
JiayiH74 <jiayih12@nycstudents.net>
2024-12-05 16:48:49 -0500

    Create Algorithms.java

---
+++ Algorithms.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Algorithms {
+    private static File f;
+    private static Scanner s; //odds
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
