e7cba55874626742e95ad4171ec6975bb7b3f657
JiayiH74 <jiayih12@nycstudents.net>
2024-12-06 19:04:34 +0000

    test

--- Algorithms.java
+++ Algorithms.java
@@ -1,11 +1,65 @@
+import java.util.ArrayList; //to add the palindromes
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileNotFoundException; 
 
 public class Algorithms {
-    private static File f;
-    private static Scanner s; //odds
+    private static File f; //type for the file, variable
+    private static Scanner s; //for reading the file
     public static void main(String[] args) throws FileNotFoundException {
 
+        f = new File("words.txt");
+        int twoLetter = findtwoLetter();
+        System.out.println(twoLetter);
+        s.close();
+
+        System.out.println(findlongWords());
+
+    }
+
+    public static int findtwoLetter() throws FileNotFoundException{
+        s = new Scanner(f);
+        int twoLetter = 0;
+        String word = "";
+        while (s.hasNext())
+        {
+            word = s.next();
+            if (word.length() == 2)
+            {
+                twoLetter++;
+            }
+        }
+        return twoLetter;
+    }
+
+    public static int findlongwordLength() throws FileNotFoundException{
+        s = new Scanner(f);
+        int longestLeng = 0;
+        while (s.hasNext())
+        {
+            int wordLeng = (s.next()).length();
+            if (wordLeng > longestLeng)
+            {
+                longestLeng = wordLeng;
+            }
+        }
+        return longestLeng;
+    }
+
+    public static ArrayList<String> findlongWords() throws FileNotFoundException{ 
+        s = new Scanner(f);
+        int longestLeng = findlongwordLength();
+        ArrayList<String> words = new ArrayList<String>();
+
+        while (s.hasNext())
+        {
+            String word = s.next();
+            int wordLeng = (s.next()).length();
+            if (wordLeng == longestLeng)
+            {
+                words.add(word);
+            }
+        }
+        return words;
     }
 }
